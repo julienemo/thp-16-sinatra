d572713f25fad4d2cfcfe51892ca09abb564ee19
Julie K <juliette.nada@gmail.com>
2019-04-22 17:01:25 +0200

    oups mauvaise manip perdu une etape

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,6 +1,7 @@
 require_relative "gossip"
 
 class ApplicationController < Sinatra::Base
+
   get '/' do
     erb :index, locals: {gossips: Gossip.all}
   end
@@ -12,6 +13,10 @@ class ApplicationController < Sinatra::Base
   post '/gossips/new/' do
     Gossip.new(params['author'].capitalize, params['content'].capitalize).save
     redirect '/'
+  end
+
+  post '/gossip/:id' do
+    erb :gossip_individual, locals:{id:params['id'].to_i, gossip:Gossip.find(params['id'].to_i)}
   end
 
 end
